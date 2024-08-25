5e6fe2649816281743a64f1ac8bd31d2e965a4e3
Mostafa Abdelmaged <mostafaabdelmaged98@gmail.com>
2024-08-25 22:11:48 +0300

    database deploy

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,7 +1,7 @@
 import Mongoose from "mongoose";
 
 export const dbConnection = Mongoose.connect(
-  "mongodb://127.0.0.1:27017/e-commerce"
+  "mongodb+srv://E-commerce:ycdtWbA4cDyDzh6V@cluster0.ydjbez3.mongodb.net/e-ecommercec42"
 )
   .then(() => {
     console.log("database connected successfully");
